7f3b025cb4badccf29acc1067ac0c8b2e3925476
OlozanoTamy <Olozanodeveloper@gmail.com>
2024-04-18 15:51:21 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const apiKey = "M7I2xAShQNBgHzqUkZHYKcxV3HoTaMpHi6evaaso"
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.post("/images", async (req, res) => {
+    let start = req.body["start-date"];
+    let end = req.body["end-date"];
+    const response = await axios.get("https://api.nasa.gov/neo/rest/v1/feed", {
+        params: {
+            start_date: start,
+            end_date: end,
+            api_key: apiKey,
+        }
+    });
+    let frase = JSON.stringify(response.data);
+    console.log(response.data['near_earth_objects'][start][0]);
+})
+app.listen(port, () => {
+    console.log(`The server is on ${port} port`)
+})
